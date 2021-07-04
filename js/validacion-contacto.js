f3d68a3fbd1d388170af02fb3c274ab64e78b64f
Matias <matiasrome132@gmail.com>
2021-07-04 19:37:14 -0300

    Validacion telefono

--- js/validacion-contacto.js
+++ js/validacion-contacto.js
@@ -1,4 +1,28 @@
+function contador(idtextarea, idcontador){
+	function update_contador(idtextarea, idcontador){
+		var contador = $(idcontador);
+		var ta = $(idtextarea);
+
+		contador.html("Caracteres restantes: "+ (1000-ta.val().length));
+	}
+
+	$(idtextarea).keyup(function(){
+		update_contador(idtextarea, idcontador)
+	});
+
+	$(idtextarea).change(function(){
+		update_contador(idtextarea, idcontador);
+	});
+}
+
+
 $(document).ready(function () {
+
+	$.validator.addMethod("telefono", function(value, element, regexp) {
+		var re = /^[0-9]{4}-[0-9]{4}$/;
+		return this.optional(element) || re.test(value);
+		}, "");
+
     $("#mensaje").hide();
 	$("#contacto").validate({
 		event: "blur",rules: 
@@ -9,13 +33,17 @@ $(document).ready(function () {
 				required: true,
 				email:true
 			},
+			'telefono': {
+				required: true,
+				telefono: true
+			},
 			'curso': "required",
             'consulta': {
                 required: true,
-                maxlength: 100
+                maxlength: 1000
             }
 		},
-		messages: {'nombre': "Ingrese su nombre", 'curso': "Seleccione un curso", 'correo': { required: 'Debe ingresar un email', email: 'Debe ingresar un email válido' },'consulta': { required: 'Debe ingresar su consulta', maxlength: 'No mas de 100 caracteres' }},
+		messages: {'telefono': { required:"Ingrese su número de teléfono", telefono:"Ingrese su número en este formato: 1111-1111"},'nombre': "Ingrese su nombre", 'curso': "Seleccione un curso", 'correo': { required: 'Debe ingresar un email', email: 'Debe ingresar un email válido' },'consulta': { required: 'Debe ingresar su consulta', maxlength: 'No mas de 100 caracteres' }},
 		debug: true,errorElement: "label",errorContainer: $("#errores"),
 		submitHandler: function(form){
 			$("#mensaje").show();
@@ -23,4 +51,6 @@ $(document).ready(function () {
 			form.submit();
 		}
 	});;
+
+	contador("#consulta","#caracteres");
 });
