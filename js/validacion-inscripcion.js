ef7994ed0f661470a13030b22fae2f69b4347f3d
Sebastian <sebastianruedaolarte@gmail.com>
2021-07-07 20:48:04 -0300

    dialog inscriptos

--- js/validacion-inscripcion.js
+++ js/validacion-inscripcion.js
@@ -1,7 +1,7 @@
-const IDAGREGADO="agregado-";
-const PRECIOCURSO=6000;
-var contador=0;
-var precio=6000;
+const IDAGREGADO = "agregado-";
+const PRECIOCURSO = 6000;
+var contador = 0;
+var precio = 6000;
 
 $(document).ready(function () {
 
@@ -11,79 +11,105 @@ $(document).ready(function () {
 
 	// Set precio base
 	setPrecio();
-   
+
 	// Clonar
-	$(".agregar-persona").click(function(){
+	$(".agregar-persona").click(function () {
 		sumarPrecio();
 		contador++;
-		var clonar=$("#persona").clone();
-		$(clonar).attr("id",IDAGREGADO+contador);
+		var clonar = $("#persona").clone();
+		$(clonar).attr("id", IDAGREGADO + contador);
 		$(".personas-agregadas").after(clonar);
 		setPrecio();
 	});
-	
+
 	// Remover
-    $(".sacar-persona").click(function(){
-        $("#agregado-"+contador).remove();
+	$(".sacar-persona").click(function () {
+		$("#agregado-" + contador).remove();
 		contador--;
 		restarPrecio();
 		setPrecio();
-     });
+	});
 
 
 
-  	$("#mensaje").hide();
+	$("#mensaje").hide();
 	$("#inscripcion").validate({
-		
+
 		event: "blur",
-		rules: 
+		rules:
 		{
 			nombre: "required",
 			apellido: "required",
-			
+
 			nombre2: "required"
 		},
-		highlight: function(element) {
-			
+		highlight: function (element) {
+
 			$("input[name='dni']").addClass('error');
 			$("input[name='apellido']").addClass('error');
 			$("input[name='nombre']").addClass('error');
-			},
-			unhighlight: function(element) {
-			
+		},
+		unhighlight: function (element) {
+
 			$("input[name='dni']").removeClass('error');
 			$("input[name='apellido']").removeClass('error');
 			$("input[name='nombre']").removeClass('error');
-			},
-			messages: {'nombre':"",'apellido':"", 'dni':""},
-		debug: true,errorElement: "div",
-		submitHandler: function(form){
-      $("#mensaje").show();
+		},
+		messages: { 'nombre': "", 'apellido': "", 'dni': "" },
+		debug: true, errorElement: "div",
+		submitHandler: function (form) {
+			$("#mensaje").show();
 			$("#mensaje").html("<p class='pensando'>Enviando el formulario, por favor espere...</p>");
-			form.submit();
+			
+			showDialog(form)
+
+			//form.submit();
 		}
 	});;
 
 
 
-	
+
 });
 
-function clonarDefault(){
-	var clonar=$("#persona").clone();
+function clonarDefault() {
+	var clonar = $("#persona").clone();
 	$(".personas-agregadas").after(clonar);
 }
 
-function setPrecio(){
-	$(".precio").text("$"+precio);
+function setPrecio() {
+	$(".precio").text("$" + precio);
 }
 
-function sumarPrecio(){
-	this.precio+=6000;
+function sumarPrecio() {
+	this.precio += 6000;
 }
 
-function restarPrecio(){
-	if (precio>6000){
-		precio-=6000;
+function restarPrecio() {
+	if (precio > 6000) {
+		precio -= 6000;
 	}
+}
+
+function showDialog(form) {
+	let dialog = new bootstrap.Modal($("#dialog-persons"))
+
+	let container = document.querySelector("#users-containers")
+
+	// si llego hasta acá es porque todos los campos están completos entonces todos los array van a tener el mismo length
+	let firstNames = document.querySelectorAll('input[name=nombre]')
+	let lastNames = document.querySelectorAll('input[name=apellido]')
+	let dnis = document.querySelectorAll('input[name=dni]')
+
+	for (let i = 0; i < firstNames.length; i++) {
+		container.innerHTML += `<p>${ firstNames[i].value } ${ lastNames[i].value } ${ dnis[i].value }</p>`
+	}
+
+	let button = document.querySelector("#button-dialog")
+	button.addEventListener("click", () => {
+		dialog.hide()
+		form.submit()
+	})
+
+	dialog.show()
 }
